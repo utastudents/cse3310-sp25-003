1693a543c7a8721edd920556c4359dab00655475
ceh6514 <ceh6514@mavs.uta.edu>
2025-04-01 21:39:35 -0500

    Declared methods isDark() and isDiagonal()

--- checkers/src/main/java/uta/cse3310/GamePlay/GamePlay.java
+++ checkers/src/main/java/uta/cse3310/GamePlay/GamePlay.java
@@ -2,9 +2,21 @@ package uta.cse3310.GamePlay;
 
 // *** Temporary
 class Move {}
-class Peice {}
+class Pieces {}
 
 public class GamePlay {
+	private boolean dark = false;
+	private boolean diagonal = false;
+
+	private boolean isDark(Move move) {
+		//Checks if the intended move spot is dark
+		return false;
+	}
+	private boolean isDiagonal(Pieces[][] table, Move move) {
+		//Checks if the intended move is diagonal from its current position
+		return false;
+	}
+
 	private void applyMove(Pieces[][] table, Move move) {}
 	private void notifyTermination(Pieces[][] table) {}
 }
