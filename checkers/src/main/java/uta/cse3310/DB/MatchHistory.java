a1c042ff12e4999e2483dd34a505cd6a83b77a58
IbrahimAli25 <Ibrahim.jaffer.ali@gmail.com>
2025-04-02 22:04:30 -0500

    added the prototype function and all variables for design

--- checkers/src/main/java/uta/cse3310/DB/MatchHistory.java
+++ checkers/src/main/java/uta/cse3310/DB/MatchHistory.java
@@ -2,4 +2,25 @@ package uta.cse3310.DB;
 
 public class MatchHistory {
     
+    private int playerID;
+    private String boardState;
+
+    public int getplayerID(int playerID){
+
+        return playerID;
+    }
+
+    public void setplayerID(int playerID){}
+
+    public String getboardState(String playerID){
+
+        return boardState;
+    }
+
+    public void setboardState(){}
+
+    public void saveGame(){}
+
+    public void loadGame(){}
 }
+
--- checkers/src/main/java/uta/cse3310/DB/PlayerInfo.java
+++ checkers/src/main/java/uta/cse3310/DB/PlayerInfo.java
@@ -1,5 +1,73 @@
 package uta.cse3310.DB;
+import java.util.Vector;
 
 public class PlayerInfo {
     
+    private String userName;
+    private String passWord;
+    private String email;
+    private Vector<Boolean> winLoss = new Vector<>();
+    private int wins;
+    private int losses;
+    private double elo;
+
+    public String getUserName(String name){
+
+        return userName;
+    }
+
+    public void setUserName(String name){
+
+    }
+
+    public String getPassWord(String pass){
+
+        return passWord;
+    }
+
+    public void setPassWord(String pass){
+
+    }
+
+    public String getEmail(String email){
+
+        return email;
+    }
+
+    public void setEmail(String email){
+
+    }
+
+    public int getWins(int wins){
+
+        return wins;
+    }
+
+    public void setWins(int wins){
+
+    }
+
+    public int getLosses(int losses){
+
+        return losses;
+    }
+
+    public void setLosses(int losses){
+
+    }
+
+    public double getElo(double elo){
+
+        return elo;
+    }
+
+    public void setElo(double elo){
+
+    }
+
+    public double calculateElo(int win , int loss){
+
+        return elo;
+    }
+
 }
