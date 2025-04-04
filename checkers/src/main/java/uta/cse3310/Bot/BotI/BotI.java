aeaed711ac1e29c00ba0b3219282cfb6ab371af2
Brant <103614931+infophysix@users.noreply.github.com>
2025-04-04 15:31:16 -0500

    Add methods to Bot I

--- checkers/src/main/java/uta/cse3310/Bot/BotI/BotI.java
+++ checkers/src/main/java/uta/cse3310/Bot/BotI/BotI.java
@@ -1,9 +1,27 @@
 package uta.cse3310.Bot.BotI;
+
+import java.util.ArrayList;
+
 class Pieces {};
 class Move {};
 
 public class BotI {
 
     public BotI() {
+    }
+    
+    private boolean validateMove(Pieces currentBoard, Move m) {
+		return false;
+    }
+
+    private ArrayList<Move> getAvailableMoves(Pieces currentBoard) {
+		return null;
+	}
+
+    private Move generateMove(Pieces currentBoard) {
+		return null;
+	}
+
+    public void onUserMove(Pieces currentBoard, Move userMove) {
     }
 }
