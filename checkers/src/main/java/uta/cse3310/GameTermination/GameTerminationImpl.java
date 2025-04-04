db769e409a518ee6810bdff680639ed6c9dd9dc5
SubashGyawali-lab <sxg3541@mavs.uta.edu>
2025-04-03 23:40:27 -0500

    feat: implimented reset and TerminationReason functions

--- checkers/src/main/java/uta/cse3310/GameTermination/GameTerminationImpl.java
+++ checkers/src/main/java/uta/cse3310/GameTermination/GameTerminationImpl.java
@@ -4,8 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Implements the game termination logic as defined by the GameTermination interface.
+ * Implements the game termination logic as defined by the GameTermination
+ * interface.
  */
 public class GameTerminationImpl implements GameTermination {
 
+    @Override
+    public void reset() {
+        boardStateHistory.clear();
+        terminationReason = TerminationReason.ONGOING;
+    }
+
+    @Override
+    public TerminationReason getTerminationReason() {
+        return terminationReason;
+    }
 }
