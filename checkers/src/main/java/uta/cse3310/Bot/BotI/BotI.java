1798b1e8a3b05d75c6387f740e9c8975f5c98a08
dhakalrupesh2345 <157661516+dhakalrupesh2345@users.noreply.github.com>
2025-04-04 18:08:37 -0500

    fix: unknown character error on Bot1.java

--- checkers/src/main/java/uta/cse3310/Bot/BotI/BotI.java
+++ checkers/src/main/java/uta/cse3310/Bot/BotI/BotI.java
@@ -2,35 +2,37 @@ package uta.cse3310.Bot.BotI;
 
 import java.util.ArrayList;
 
-class Pieces {};
-class Move {};
+class Pieces {
+};
+
+class Move {
+};
 
 public class BotI {
 
-    public BotI() {
-    }
-    
-    private boolean validateMove(Pieces currentBoard, Move m) {
-		return false;
-    }
+	public BotI() {
+	}
 
-    private ArrayList<Move> getAvailableMoves(Pieces currentBoard) {
-	    ArrayList<Move> availableMoves = new ArrayList<Move>();
+	private boolean validateMove(Pieces currentBoard, Move m) {
+		return false;
+	}
+
+	private ArrayList<Move> getAvailableMoves(Pieces currentBoard) {
+		ArrayList<Move> availableMoves = new ArrayList<Move>();
 		return availableMoves;
 	}
 
-    private Move generateMove(Pieces currentBoard) {
-	    ArrayList<Move> availableMoves = getAvailableMoves(currentBoard);
-	    Move tmp = new Move();
+	private Move generateMove(Pieces currentBoard) {
+		ArrayList<Move> availableMoves = getAvailableMoves(currentBoard);
+		Move tmp = new Move();
 		return tmp;
 	}
 
-    public void onUserMove(Pieces currentBoard, Move userMove) {
-	    Move botMove = generateMove(currentBoard);
-	    if (botMove == null){
-		    |
-	    } else {
-		    
-	    }
-    }
+	public void onUserMove(Pieces currentBoard, Move userMove) {
+		Move botMove = generateMove(currentBoard);
+		if (botMove == null) {
+
+		} else {
+		}
+	}
 }
