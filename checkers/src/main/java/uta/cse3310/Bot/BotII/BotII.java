7d3e4e68004a81e51fe938299e5061830cfa9472
vxt1516 <vxt1516@mavs.uta.edu>
2025-04-04 23:57:26 -0500

    PRIORITIZE DEFENSE

--- checkers/src/main/java/uta/cse3310/Bot/BotII/BotII.java
+++ checkers/src/main/java/uta/cse3310/Bot/BotII/BotII.java
@@ -1,5 +1,22 @@
 package uta.cse3310.Bot.BotII;
 
 public class BotII {
-    
+    private void prioritizeDefense(BoardState boardState) 
+    {
+        ArrayList<Move> legalMoves = getLegalMoves(boardState);
+        
+        Move blockMove = findBlockMove(legalMoves);
+        if (blockMove != null) 
+        {
+            executeMove(blockMove);
+            return;
+        }
+
+        Move safeMove = findSafeMove(legalMoves);
+        if (safeMove != null) 
+        {
+            executeMove(safeMove);
+        }
+        
+    }
 }
