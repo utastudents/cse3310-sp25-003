d4553eb84f6fbd5202d7f28c1e2ac9f9b4d05b7f
Aditya2024-coder <adityarauniyar2024@gmail.com>
2025-04-05 00:20:58 -0500

    Update BotII.java

--- checkers/src/main/java/uta/cse3310/Bot/BotII/BotII.java
+++ checkers/src/main/java/uta/cse3310/Bot/BotII/BotII.java
@@ -19,4 +19,27 @@ public class BotII {
         }
         
     }
+    //Evaluate the board and return a score based on the amount of pieces, kings, and center control
+    public int evaluateBoard(BoardState boardState) {
+        int score = 0;
+
+        //Score for number of bot pieces and opponent pieces
+        score += 10 * countBotPieces(boardState);
+        score -= 10 * countOpponentPieces(boardState);
+
+        //Score for number of kings
+        score += 20 * countBotKings(boardState);
+        score -= 20 * countOpponentKings(boardState);
+
+        //Evaluate center control: Number of pieces in the center
+        score += 15 * countPiecesInCenter(boardState);
+
+        return score;
+    }
+
+    //Check if there's an opportunity to capture an opponent piece
+    private boolean findCaptureOpportunity(BoardState boardState) {
+        //Capture Move logic will be added here.
+        return true;
+    }
 }
