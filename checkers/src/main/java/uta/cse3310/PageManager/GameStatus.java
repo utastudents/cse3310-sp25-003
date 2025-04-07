c277fae92ff055ad2240100f6689d9ee70a543b2
axp9210 <axp9210@mavs.uta.edu>
2025-04-07 18:06:25 -0500

    Create the LoginPayload.java to receive JSON payload from Login team

---
+++ checkers/src/main/java/uta/cse3310/PageManager/GameStatus.java
@@ -0,0 +1,10 @@
+package uta.cse3310.PageManager;
+
+public class game_status {
+    public Integer turn;
+    public Float score;
+    public String message;
+    public String gameID;
+    public String opponent;
+}
+
---
+++ checkers/src/main/java/uta/cse3310/PageManager/LoginPayload.java
@@ -0,0 +1,6 @@
+package uta.cse3310.PageManager;
+
+public class LoginPayload {
+    public String username;
+    public String password;
+}
