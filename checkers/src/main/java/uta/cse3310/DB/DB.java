432570d955b3a8b32dfae2f88ddd24d5e5f95aff
ShawnAbe12 <126189547+ShawnAbe12@users.noreply.github.com>
2025-04-08 09:17:56 -0500

    adding insertion + created User+Match database + made Username and Email unique + formatted code

--- checkers/src/main/java/uta/cse3310/DB/DB.java
+++ checkers/src/main/java/uta/cse3310/DB/DB.java
@@ -1,3 +1,4 @@
+
 package uta.cse3310.DB;
 
 import java.sql.*;
@@ -5,27 +6,59 @@ import java.sql.*;
 // Compile with
 // javac ./DB.java
 
-// Open Database with Command below
-// java -classpath ".;sqlite-jdbc-3.7.2.jar" DB.java
+// Open Databases with Command below
+// java -classpath ".;sqlite-jdbc-3.7.2.jar" ./DB.java
 
 
 public class DB {
+
   public static void main( String args[] ) {
+
       Connection c = null;
       Statement stmt = null;
       
       try {
-         Class.forName("org.sqlite.JDBC");
-         c = DriverManager.getConnection("jdbc:sqlite:Database.db");
-         c.setAutoCommit(false);
+         // Create a Connection to the database or create database if not found
+         c = initConnection();
          stmt = c.createStatement();
 
-         System.out.println("Opened database successfully");
+         // ******   CREATE USER + MATCH DATABASE      ******
+         // ****** ONLY USE WHEN DATABASE IS CORRUPTED ******
+         // createDatabase(stmt);
 
+         String userName = "";
+         String email = "";
+         String password= "";
+
+         initUser(stmt,userName,email,password);
+
+         
+      } catch ( Exception e ) {
+         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+         System.exit(0);
+      }
+
+   }
+   public static Connection initConnection() throws SQLException, ClassNotFoundException{
+      Connection c = null;
+      Class.forName("org.sqlite.JDBC");
+      c = DriverManager.getConnection("jdbc:sqlite:Database.db");
+      c.setAutoCommit(false);
+      return c
+
+   }
+   public static void closeConnection(Connection c, Statement stmt) throws SQLException{
+      stmt.close();
+      c.commit();
+      c.close();
+   }
+   @SuppressWarnings("unused")
+   private static void createDatabase(Statement stmt) throws SQLException{
          String sql = "CREATE TABLE USER_DATABASE " +
                         "(PLAYERID       INT     PRIMARY KEY     NOT NULL," +
-                        " USERNAME       TEXT    NOT NULL," + 
-                        " EMAIL          TEXT    NOT NULL," + 
+                        " USERNAME       TEXT    UNIQUE          NOT NULL," + 
+                        " EMAIL          TEXT    UNIQUE          NOT NULL," + 
+                        " PASSWORD       TEXT    NOT NULL," +
                         " WINS           INT     NOT NULL," +
                         " LOSSES         INT     NOT NULL)"; 
          stmt.executeUpdate(sql);
@@ -44,29 +77,11 @@ public class DB {
          System.out.println("MATCH DATABASE CREATED");
 
 
-         // sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
-         //                "VALUES (1, 'Paul', 32, 'California', 20000.00 );"; 
-         // stmt.executeUpdate(sql);
-
-         // sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
-         //          "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );"; 
-         // stmt.executeUpdate(sql);
-
-         // sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
-         //          "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );"; 
-         // stmt.executeUpdate(sql);
-
-         // sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
-         //          "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );"; 
-         // stmt.executeUpdate(sql);
-
-         stmt.close();
-         c.commit();
-         c.close();
-      } catch ( Exception e ) {
-         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-         System.exit(0);
-      }
-
+   }
+   public static void initUser(Statement stmt, String userName, String email, String password) throws SQLException{
+      // PLAYERID (INT), USERNAME(STRING), EMAIL(STRING), PASSWORD(STRING), WINS(INT), LOSSES(INT)
+      String sql = "INSERT INTO USER_DATABASE (PLAYERID,USERNAME,EMAIL,PASSWORD, WINS, LOSSES) " +
+                     "VALUES (2,"+userName+","+email+","+password+", 0, 0 )"; 
+      stmt.executeUpdate(sql);
    }
 }
