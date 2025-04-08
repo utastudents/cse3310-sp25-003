4d976fd7411927ab548d3b0dfa01e8fb8ddbde8b
ShawnAbe12 <126189547+ShawnAbe12@users.noreply.github.com>
2025-04-07 21:59:53 -0500

    Created User and Match Database, will work on inserting later

--- checkers/src/main/java/uta/cse3310/DB/DB.java
+++ checkers/src/main/java/uta/cse3310/DB/DB.java
@@ -12,15 +12,61 @@ import java.sql.*;
 public class DB {
   public static void main( String args[] ) {
       Connection c = null;
+      Statement stmt = null;
       
       try {
-
-        Class.forName("org.sqlite.JDBC");
+         Class.forName("org.sqlite.JDBC");
          c = DriverManager.getConnection("jdbc:sqlite:Database.db");
+         c.setAutoCommit(false);
+         stmt = c.createStatement();
+
+         System.out.println("Opened database successfully");
+
+         String sql = "CREATE TABLE USER_DATABASE " +
+                        "(PLAYERID       INT     PRIMARY KEY     NOT NULL," +
+                        " USERNAME       TEXT    NOT NULL," + 
+                        " EMAIL          TEXT    NOT NULL," + 
+                        " WINS           INT     NOT NULL," +
+                        " LOSSES         INT     NOT NULL)"; 
+         stmt.executeUpdate(sql);
+
+         System.out.println("USER DATABASE CREATED");
+
+         sql = "CREATE TABLE MATCH_DATABASE " +
+                        "(MATCHID        INT     PRIMARY KEY     NOT NULL," + 
+                        " BLACKPLAYERID  INT     NOT NULL," +
+                        " WHITEPLAYERID  INT     NOT NULL," +
+                        " BOARDSTATE     TEXT    NOT NULL," + 
+                        " WINNER         INT     NOT NULL," +
+                        " LOSER          INT     NOT NULL)"; 
+         
+         stmt.executeUpdate(sql);
+         System.out.println("MATCH DATABASE CREATED");
+
+
+         // sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+         //                "VALUES (1, 'Paul', 32, 'California', 20000.00 );"; 
+         // stmt.executeUpdate(sql);
+
+         // sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+         //          "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );"; 
+         // stmt.executeUpdate(sql);
+
+         // sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+         //          "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );"; 
+         // stmt.executeUpdate(sql);
+
+         // sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+         //          "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );"; 
+         // stmt.executeUpdate(sql);
+
+         stmt.close();
+         c.commit();
+         c.close();
       } catch ( Exception e ) {
          System.err.println( e.getClass().getName() + ": " + e.getMessage() );
          System.exit(0);
       }
-      System.out.println("Opened database successfully");
+
    }
 }
