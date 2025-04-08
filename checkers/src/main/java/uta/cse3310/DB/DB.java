cba3855c494c79423f2e860b2b2faecbd767e3b4
ShawnAbe12 <126189547+ShawnAbe12@users.noreply.github.com>
2025-04-07 20:00:48 -0500

    adding base database w sqlite + saving connection

--- checkers/src/main/java/uta/cse3310/DB/DB.java
+++ checkers/src/main/java/uta/cse3310/DB/DB.java
@@ -1,5 +1,26 @@
 package uta.cse3310.DB;
 
-public class DB {
+import java.sql.*;
 
+// Compile with
+// javac ./DB.java
+
+// Open Database with Command below
+// java -classpath ".;sqlite-jdbc-3.7.2.jar" DB.java
+
+
+public class DB {
+  public static void main( String args[] ) {
+      Connection c = null;
+      
+      try {
+
+        Class.forName("org.sqlite.JDBC");
+         c = DriverManager.getConnection("jdbc:sqlite:Database.db");
+      } catch ( Exception e ) {
+         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+         System.exit(0);
+      }
+      System.out.println("Opened database successfully");
+   }
 }
