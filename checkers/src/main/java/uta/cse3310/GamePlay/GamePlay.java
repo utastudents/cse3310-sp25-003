541474a4f5fe1438f8e9db3e6c47471f44fa6178
ceh6514 <ceh6514@mavs.uta.edu>
2025-04-10 19:03:37 -0500

    Implemented isInBounds() and isDiagonal() and removed notifyTermination()

--- checkers/src/main/java/uta/cse3310/GamePlay/GamePlay.java
+++ checkers/src/main/java/uta/cse3310/GamePlay/GamePlay.java
@@ -1,44 +1,48 @@
 package uta.cse3310.GamePlay;
-
-// *** Temporary
-class Move {}
-class Pieces {}
+import uta.cse3310.GameManager.Move;
+import uta.cse3310.GameManager.GameState;
 
 public class GamePlay {
-	private boolean dark = false;
-	private boolean diagonal = false;
 
-	private boolean isDark(Move move) {
-		//Checks if the intended move spot is dark
-		return false;
-	}
-	private boolean isDiagonal(Pieces[][] table, Move move) {
-		//Checks if the intended move is diagonal from its current position
-		return false;
+	private boolean isInBounds(Move move){//Checks if the coords of the "to" posititon is in the bounds of the array
+	    if(move.getTo().getX() >= 0 && move.getTo().getX() <= 7){ //If X and Y are both within 0-7
+	        if(move.getTo().getY() >= 0 && move.getTo().getY() <= 7){
+	            return true;
+	        }
+	    } 
+	    return false;
 	}
 
-	private void applyMove(Pieces[][] table, Move move) {}
-	private void notifyTermination(Pieces[][] table) {}
+	private boolean isDiagonal(Move move) { //Checks if the intended move is diagonal from its current position
+		if(Math.abs(move.getTo().getX() - move.getFrom().getX()) == 1){ //If X2 - X1 && Y2 - Y1 both are equal to 1
+		    if(Math.abs(move.getTo().getY() - move.getFrom().getY()) == 1){
+		        return true;
+		    }
+		}
+		return false;
+	}
 
-	private boolean isSameTeam(Pieces[][] table, Move move) 
+	private void applyMove(GameState boardState, Move move) {}
+
+	private boolean isSameTeam(GameState boardState, Move move) 
 	{
     	return false;
 	}
 
-	private void promotePiece(Pieces[][] table, Move move) {
+	private void promotePiece(GameState boardState, Move move) {
         	//Implementation would check if the piece should be promoted and modify the piece accordingly.
     	}
 
-	private boolean isEmpty(Pieces[][] table, int row, int col) {
+	private boolean isEmpty(GameState boardState, int row, int col) {
         	//Would return true if table[row][col] is empty.
         	return false;
     	}
 	
-	public boolean isValidMove(Pieces[][] table, Move move) 
+	public boolean isValidMove(GameState boardState, Move move) 
 	{
     	return false;
 	}
-	public boolean isJump(Pieces[][] table, Move move){
+	public boolean isJump(GameState boardState, Move move){
 		//Would return true if intended move intended move captures an enemy piece is valid
 		return false;
 	}
