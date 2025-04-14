b57404f0252c51630e8e5a2f457e822e9fffc590
IbrahimAli25 <ija8624@mavs.uta.edu>
2025-04-14 15:41:02 -0500

    added getter for playerID

--- checkers/src/main/java/uta/cse3310/DB/MatchHistory.java
+++ checkers/src/main/java/uta/cse3310/DB/MatchHistory.java
@@ -1,13 +1,27 @@
 package uta.cse3310.DB;
 
+import java.sql.*;
+
 public class MatchHistory {
     
     private int playerID;
     private String boardState;
 
-    public int getplayerID(int playerID){
 
-        return playerID;
+
+    public int getplayerID(String userName) throws SQLException , ClassNotFoundException{
+
+        Connection c = null;
+        Class.forName("org.sqlite.JDBC");
+        c = DriverManager.getConnection("jdbc:sqlite:uta\\cse3310\\DB\\Database.db");
+        c.setAutoCommit(false);
+        Statement stmt = c.createStatement();
+      
+        String sql = "SELECT * FROM USER_DATABASE WHERE USERNAME == "+userName+"";
+        ResultSet rs = stmt.executeQuery(sql);
+
+        int result = rs.getInt("PLAYERID");
+        return result;
     }
 
     public void setplayerID(int playerID){}
