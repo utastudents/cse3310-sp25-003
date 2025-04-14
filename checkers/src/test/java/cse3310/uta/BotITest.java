37eab378ca981dc5c54a776171993ae10a7f6c82
infophysix <github@wirehead.dev>
2025-04-14 14:31:18 -0500

    Add BotITest.java

---
+++ checkers/src/test/java/cse3310/uta/BotITest.java
@@ -0,0 +1,19 @@
+package cse3310.uta;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import uta.cse3310.Bot.BotI.*;
+
+// Unit tests for Bot I
+public class BotITest 
+{
+    //TC-001: Test getAvailableStandardMoves()
+    @Test
+    public void testGetAvailableStandardMoves()
+    {
+		// TODO: Actually write this test lol
+        assertTrue(true);
+    }
+}
