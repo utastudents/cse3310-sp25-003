22b8856ca7cec749ba19725467a1ecc30cabf903
uta-1002062105 <uta-1002062105@github.com>
2025-04-14 21:33:36 -0400

    New js file, and position absolute on leaderboard

---
+++ checkers/src/main/webapp/Leaderboard/summary.js
@@ -0,0 +1,10 @@
+function showContent(){
+    document.querySelector(".hide").style.display = "block";
+    document.querySelector(".show").style.display = "none";
+}
+
+function hideContent(){
+    document.querySelector(".hide").style.display = "none";
+    document.querySelector(".show").style.display = "flex";
+}
+
