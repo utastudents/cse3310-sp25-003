47938947e746f40d86d162c602fd72a0dc707cf7
John Garcia <john.garcia3@mavs.uta.edu>
2025-04-15 13:59:10 -0500

    jump

--- checkers/src/main/java/uta/cse3310/GamePlay/GamePlay.java
+++ checkers/src/main/java/uta/cse3310/GamePlay/GamePlay.java
@@ -1,4 +1,5 @@
 package uta.cse3310.GamePlay;
+
 import uta.cse3310.GameManager.Move;
 import uta.cse3310.GameManager.GameState;
 import uta.cse3310.GameManager.Position;
@@ -10,61 +11,92 @@ import uta.cse3310.GameTermination.IGameTermination.Piece;
 
 public class GamePlay {
 
-	public boolean isInBounds(Move move){//Checks if the coords of the "to" posititon is in the bounds of the array
-	    if(move.getTo().getX() >= 0 && move.getTo().getX() <= 7){ //If X and Y are both within 0-7
-	        if(move.getTo().getY() >= 0 && move.getTo().getY() <= 7){
-	            return true;
-	        }
-	    } 
-	    return false;
+	public boolean isInBounds(Move move) {// Checks if the coords of the "to" posititon is in the bounds of the array
+		if (move.getTo().getX() >= 0 && move.getTo().getX() <= 7) { // If X and Y are both within 0-7
+			if (move.getTo().getY() >= 0 && move.getTo().getY() <= 7) {
+				return true;
+			}
+		}
+		return false;
 	}
 
-	public boolean isDiagonal(Move move) { //Checks if the intended move is diagonal from its current position
-		if(Math.abs(move.getTo().getX() - move.getFrom().getX()) == 1){ //If X2 - X1 && Y2 - Y1 both are equal to 1
-		    if(Math.abs(move.getTo().getY() - move.getFrom().getY()) == 1){
-		        return true;
-		    }
+	public boolean isDiagonal(Move move) { // Checks if the intended move is diagonal from its current position
+		if (Math.abs(move.getTo().getX() - move.getFrom().getX()) == 1) { // If X2 - X1 && Y2 - Y1 both are equal to 1
+			if (Math.abs(move.getTo().getY() - move.getFrom().getY()) == 1) {
+				return true;
+			}
 		}
 		return false;
 	}
 
 	private void applyMove(GameState boardState, Move move) {
 		Position from = new Position(move.getFrom().getX(),
-					move.getFrom().getY());
+				move.getFrom().getY());
 		Position to = new Position(move.getTo().getX(),
-					move.getTo().getY());
+				move.getTo().getY());
 
 		boardState.applyMove(from, to);
 	}
 
-	private boolean isSameTeam(GameState boardState, Move move) 
-	{
-    	Piece fromPiece = boardState.getPieceAt(move.getFrom());
-    	Piece toPiece = boardState.getPieceAt(move.getTo());
+	private boolean isSameTeam(GameState boardState, Move move) {
+		Piece fromPiece = boardState.getPieceAt(move.getFrom());
+		Piece toPiece = boardState.getPieceAt(move.getTo());
 
-    	if (fromPiece == null || toPiece == null) 
-			{
-        		return false;
-    		}
+		if (fromPiece == null || toPiece == null) {
+			return false;
+		}
 
-    	return fromPiece.getColor() == toPiece.getColor(); //piece color class from iGameTermination 
+		return fromPiece.getColor() == toPiece.getColor(); // piece color class from iGameTermination
 	}
 
 	private void promotePiece(GameState boardState, Move move) {
-        	//Implementation would check if the piece should be promoted and modify the piece accordingly.
-    	}
+		// Implementation would check if the piece should be promoted and modify the
+		// piece accordingly.
+	}
 
 	private boolean isEmpty(GameState boardState, int row, int col) {
-        	//Would return true if table[row][col] is empty.
-        	return false;
-    	}
-	
-	public boolean isValidMove(GameState boardState, Move move) 
-	{
-    	return false;
+		// Would return true if table[row][col] is empty.
+		return false;
 	}
-	public boolean isJump(GameState boardState, Move move){
-		//Would return true if intended move intended move captures an enemy piece is valid
+
+	public boolean isValidMove(GameState boardState, Move move) {
+		return false;
+	}
+
+	public boolean isJump(GameState boardState, Move move) {
+		// Would return true if intended move intended move captures an enemy piece is
+		// valid
+		return false;
+	}
+
+	public boolean isJump(GameState boardState, Move move) {// Would return true if intended move intended move captures
+															// an enemy piece is valid
+		boolean jump2;
+		boolean empty;
+		boolean teammate;
+		boolean isKing = false; // Placeholder until I know when to use a king piece
+
+		if (isKing == true) {
+			if (Math.abs(move.getTo().getX() - move.getFrom().getX()) == 2) { // If X2 - X1 && Y2 - Y1 both are equal to
+																				// 2
+				if (Math.abs(move.getTo().getY() - move.getFrom().getY()) == 2) {
+					jump2 = true;
+				}
+			}
+		} else {
+			jump2 = false;
+		}
+
+		if (isKing = false) {
+			if (Math.abs(move.getTo().getX() - move.getFrom().getX()) == 2) {// X value can the absolute value of 2 but
+																				// Y cannot
+				if (move.getTo().getY() - move.getFrom().getY() == 2) {
+					jump2 = true;
+				}
+			}
+		} else {
+			jump2 = false;
+		}
 		return false;
 	}
 }
