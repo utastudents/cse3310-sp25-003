fb902b2ac4409c41ec7fe93c67e4442ea4094bb9
axk6637 <axk6637@mavs.uta.edu>
2025-04-15 11:19:24 -0500

    password validation added

--- checkers/src/main/webapp/validation.js
+++ checkers/src/main/webapp/validation.js
@@ -1,5 +1,6 @@
-
 class inputValidator{
+    //username validation
+    //Requirement [3,4,5]
     static validateUsername(username){
         
         if (!username) 
@@ -13,4 +14,28 @@ class inputValidator{
             return "Username cannot contain spaces.";
 
     }
+
+    //password validation using regex
+    //Requirement [6,7]
+    static validatePassword(password){
+        if (!password)
+            return "Password cannot be empty.";
+        if (password.length <8) 
+            return "Password must be 8 characters long.";
+        if (!/ [A-Z]/.test(password))
+            return "Password must contain at least one uppecare letter.";
+        if (!/[a-z]/.test(password))
+            return "Password must contain at least one lowwercase letter.";
+        if (!/[0-9]/.test(password))
+            return "Password must contain at least one number.";
+        if (!/[!@#$%^&*]/.test(password))
+            return "PAssword must contain one special character (! @ # $ % ^ & *).";
+        //if (/\s/.test (password))
+            //return "Password cannot contain spaces.";
+    }
+
+    
+
+
 }
+
