15fc7a659b73008d9d8ba0cd080022bb004cfb7a
ceh6514 <ceh6514@mavs.uta.edu>
2025-04-15 00:37:43 -0500

    Re-adding GamePlay's work that was lost post-5ce41cb's commit

--- checkers/src/main/java/uta/cse3310/GamePlay/GamePlay.java
+++ checkers/src/main/java/uta/cse3310/GamePlay/GamePlay.java
@@ -1,44 +1,69 @@
 package uta.cse3310.GamePlay;
-
-// *** Temporary
-class Move {}
-class Pieces {}
+import uta.cse3310.GameManager.Move;
+import uta.cse3310.GameManager.GameState;
+import uta.cse3310.GameManager.Position;
+import uta.cse3310.GameTermination.IGameTermination.Piece;
+/* Game termination has a piece class that Game Manager is using in their Gamestate.java file. 
+	I could implement it here, but I would have to talk to Game Manager first, so for compilation
+	purposes I imported Game termination's piece class. (Written by Devyn)
+*/
 
 public class GamePlay {
-	private boolean dark = false;
-	private boolean diagonal = false;
 
-	private boolean isDark(Move move) {
-		//Checks if the intended move spot is dark
-		return false;
-	}
-	private boolean isDiagonal(Pieces[][] table, Move move) {
-		//Checks if the intended move is diagonal from its current position
-		return false;
+	public boolean isInBounds(Move move){//Checks if the coords of the "to" posititon is in the bounds of the array
+	    if(move.getTo().getX() >= 0 && move.getTo().getX() <= 7){ //If X and Y are both within 0-7
+	        if(move.getTo().getY() >= 0 && move.getTo().getY() <= 7){
+	            return true;
+	        }
+	    } 
+	    return false;
 	}
 
-	private void applyMove(Pieces[][] table, Move move) {}
-	private void notifyTermination(Pieces[][] table) {}
+	public boolean isDiagonal(Move move) { //Checks if the intended move is diagonal from its current position
+		if(Math.abs(move.getTo().getX() - move.getFrom().getX()) == 1){ //If X2 - X1 && Y2 - Y1 both are equal to 1
+		    if(Math.abs(move.getTo().getY() - move.getFrom().getY()) == 1){
+		        return true;
+		    }
+		}
+		return false;
+	}
 
-	private boolean isSameTeam(Pieces[][] table, Move move) 
+	private void applyMove(GameState boardState, Move move) {
+		Position from = new Position(move.getFrom().getX(),
+					move.getFrom().getY());
+		Position to = new Position(move.getTo().getX(),
+					move.getTo().getY());
+
+		boardState.applyMove(from, to);
+	}
+
+	private boolean isSameTeam(GameState boardState, Move move) 
 	{
-    	return false;
+    	Piece fromPiece = boardState.getPieceAt(move.getFrom());
+    	Piece toPiece = boardState.getPieceAt(move.getTo());
+
+    	if (fromPiece == null || toPiece == null) 
+			{
+        		return false;
+    		}
+
+    	return fromPiece.getColor() == toPiece.getColor(); //piece color class from iGameTermination 
 	}
 
-	private void promotePiece(Pieces[][] table, Move move) {
+	private void promotePiece(GameState boardState, Move move) {
         	//Implementation would check if the piece should be promoted and modify the piece accordingly.
     	}
 
-	private boolean isEmpty(Pieces[][] table, int row, int col) {
+	private boolean isEmpty(GameState boardState, int row, int col) {
         	//Would return true if table[row][col] is empty.
         	return false;
     	}
 	
-	public boolean isValidMove(Pieces[][] table, Move move) 
+	public boolean isValidMove(GameState boardState, Move move) 
 	{
     	return false;
 	}
-	public boolean isJump(Pieces[][] table, Move move){
+	public boolean isJump(GameState boardState, Move move){
 		//Would return true if intended move intended move captures an enemy piece is valid
 		return false;
 	}
