0d0ba28324d15a739dddffce97e30664767ff324
Oscar Guerrero <oxg8439@mavs.uta.edu>
2025-04-16 18:36:00 -0500

    Update GameManager.java
    added Pair up

--- checkers/src/main/java/uta/cse3310/GameManager/GameManager.java
+++ checkers/src/main/java/uta/cse3310/GameManager/GameManager.java
@@ -1,14 +1,14 @@
 package uta.cse3310.GameManager;
 
 import uta.cse3310.GamePlay.GamePlay;
-import uta.cse3310.GameTermination.GameTermination;
+import uta.cse3310.PairUp.PairUp;
 import uta.cse3310.Bot.BotI.BotI;
 import uta.cse3310.Bot.BotII.BotII;
 
 public class GameManager {
 
     private GamePlay gp;
-    private GameTermination gt;
+    private PairUp gt;
     private BotI b1;
     private BotII b2;
 
@@ -17,7 +17,7 @@ public class GameManager {
 
     public GameManager() {
         gp = new GamePlay();
-        gt = new GameTermination();
+        gt = new PairUp();
         b1 = new BotI(this);  // BotI expects GameManager reference
         b2 = new BotII();
     }
