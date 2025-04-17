bd6006d83ac45a31dbd54a0db43190b7ef492505
mxn0591 <mxn0591@mavs.uta.edu>
2025-04-17 16:54:15 -0500

    update PlayerInfo.java

--- checkers/src/main/java/uta/cse3310/DB/PlayerInfo.java
+++ checkers/src/main/java/uta/cse3310/DB/PlayerInfo.java
@@ -1,9 +1,14 @@
 package uta.cse3310.DB;
+// import uta.cse3310.DB.*;
+
 
 import java.util.Vector;
 
-public class PlayerInfo {
+import java.sql.*;
 
+
+public class PlayerInfo {
+    
     private String userName;
     private String passWord;
     private String email;
@@ -12,48 +17,67 @@ public class PlayerInfo {
     private int losses;
     private double elo;
 
-    public String getUserName(String name) {
+    // public PlayerInfo(){
+    //     userName = "";
+    //     passWord = "";
+    //     email = "";
+    //     wins = 0;
+    //     losses = 0;
+    //     elo = 0;
 
-        return userName;
+    // }
+
+   
+    public String getUserName(int userID) throws SQLException , ClassNotFoundException{
+        ResultSet rs = DB.getSpecificData(userID, "USER");
+        String result = rs.getString("USERNAME");
+        return result;
     }
 
-    public void setUserName(String name) {
-
-    }
-
-    public String getPassWord(String pass) {
-
-        return passWord;
-    }
-
-    public void setPassWord(String pass) {
-
-    }
-
-    public String getEmail(String email) {
-
-        return email;
-    }
-
-    public void setEmail(String email) {
+    public void setUserName(Statement stmt, String name , int userID) throws SQLException , ClassNotFoundException{
+        DB.setSpecificDataString(stmt, userID, "USERNAME", name, "USER");
 
     }
 
-    public int getWins(int wins) {
+    
+    public String getPassWord(int userID) throws SQLException , ClassNotFoundException{
+        ResultSet rs = DB.getSpecificData(userID, "USER");
+        String result = rs.getString("PASSWORD");
+        return result;
+    }
+
+    public void setPassWord(Statement stmt, String pass, int userID) throws SQLException, ClassNotFoundException {
+        DB.setSpecificDataString(stmt, userID, "PASSWORD", pass, "USER");
+    }
+
+    public static String getEmail(int userID) throws SQLException, ClassNotFoundException{
+        ResultSet rs = DB.getSpecificData(userID,"USER");
+        String result = rs.getString("EMAIL");
+        
+        return result;
+    }
+
+    public void setEmail(Statement stmt, String email, int playerID) throws SQLException, ClassNotFoundException{
+        DB.setSpecificDataString(stmt, playerID, "EMAIL", email, "USER");
+    }
+
+    public int getWins(int wins){
 
         return wins;
     }
 
-    public void setWins(int wins) {
-
+    public void setWins(int wins){
+        
+        this.wins = wins;
     }
 
-    public int getLosses(int losses) {
+    public int getLosses(int losses){
 
         return losses;
     }
 
-    public void setLosses(int losses) {
+    public void setLosses(int losses){
+        this.losses = losses;
 
     }
 
