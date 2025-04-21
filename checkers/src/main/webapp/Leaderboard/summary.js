c5dd28954f4287b3c43d978d3af76872528b51fb
Edwin Najera <eln2105@mavs.uta.edu>
2025-04-20 23:45:21 -0500

    Leaderboard reverted to display some info

--- checkers/src/main/webapp/Leaderboard/summary.js
+++ checkers/src/main/webapp/Leaderboard/summary.js
@@ -8,7 +8,7 @@ function hideContent(){
     document.querySelector(".show").style.display = "flex";
 }
 
-function handleLeaderboardData(playerData){
+function handleLeaderboardData(){
     
     const playerData = [
         
