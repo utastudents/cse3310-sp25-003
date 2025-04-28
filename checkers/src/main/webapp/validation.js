ebfb9aff63e0a159ca0ea77ab4a4722c7e1e7362
uta-ixm9432 <ixm9432@mavs.uta.edu>
2025-04-28 18:32:46 -0500

    Update validation.js
    Fixed minor spelling errors for login error messages

--- checkers/src/main/webapp/validation.js
+++ checkers/src/main/webapp/validation.js
@@ -23,9 +23,9 @@ class inputValidator{
         if (password.length <8) 
             return "Password must be at least 8 characters long.";
         if (!/[A-Z]/.test(password))
-            return "Password must contain at least one uppecare letter.";
+            return "Password must contain at least one uppercase letter.";
         if (!/[a-z]/.test(password))
-            return "Password must contain at least one lowwercase letter.";
+            return "Password must contain at least one lowercase letter.";
         if (!/[0-9]/.test(password))
             return "Password must contain at least one number.";
         if (!/[!@#$%^&*]/.test(password))
