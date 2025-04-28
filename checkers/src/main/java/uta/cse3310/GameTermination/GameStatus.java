94dc15309234953763dce8983e9ad5c89c10b709
SubashGyawali-lab <sxg3541@mavs.uta.edu>
2025-04-28 18:56:59 -0500

    added documentation for IgameTermination interface

--- checkers/src/main/java/uta/cse3310/GameTermination/GameStatus.java
+++ checkers/src/main/java/uta/cse3310/GameTermination/GameStatus.java
@@ -6,11 +6,26 @@ import java.util.Map;
 
 /**
  * Represents the possible statuses of the game.
+ * This enum is used to indicate the current state of the game.
  */
-
 public enum GameStatus {
+    /**
+     * Indicates that the black player has won the game.
+     */
     BLACK_WIN,
+
+    /**
+     * Indicates that the red player has won the game.
+     */
     RED_WIN,
+
+    /**
+     * Indicates that the game is still ongoing.
+     */
     ONGOING,
+
+    /**
+     * Indicates that the game has ended in a draw.
+     */
     DRAW
 }
